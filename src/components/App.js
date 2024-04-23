0ad5cd59db727c16c6791a36aefcb1dfdfd0762a
Immaculatewangari <immaculate.wangari@student.moringaschool.com>
2024-04-23 23:34:04 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function NavBar() {
       <a href="#home">I'm a link!</a>
     </nav>
   );
+
 }
 
 function Home() {
@@ -16,15 +17,20 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <NavBar/>
+      <Home />
+      <About />
     </div>
   );
 }
